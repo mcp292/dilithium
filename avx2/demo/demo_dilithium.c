d0c1204b7b1cc3141d2f0377c694ca8fb3329e3f
Michael Partridge <mcp292@nau.edu>
2022-10-04 15:53:29 -0700

    Refactor for certificate authority demo

--- avx2/demo/demo_dilithium.c
+++ avx2/demo/demo_dilithium.c
@@ -1,3 +1,14 @@
+/* Takes in action and depending on action takes in additional options as follows:
+   if action == "generate":
+     argument_list = ["generate", randomness/seed]
+   elif action == "sign":
+     argument_list = ["sign", private_key, message]
+   elif action == "verify":
+     argument_list = ["verify", public_key, message_hash] */
+
+/* TODO: implement this around ref implementation/dir */
+/* TODO: original demo broken after changing commandline arguments */
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -8,10 +19,6 @@
 
 #define MLEN 59
 
-/* takes in message and random_bytes as commandline arguments, in that order */
-#define MESSAGE_IND 1
-#define RANDOM_BYTES_IND 2
-
 /* taken from https://github.com/mcp292/c_utils */
 /** Convert hex string to byte array. [2.15.21]
 
@@ -19,7 +26,8 @@
     capacity. */
 void hex_to_bytes(uint8_t bytes[], char *hex)
 {
-    int ind, hex_ind, byte; /* %2x expects int. */
+    int ind, hex_ind;
+    unsigned int byte; /* %2x expects int. */
 
     for (ind = hex_ind = 0;
          hex[hex_ind] != '\0';
@@ -42,105 +50,178 @@ void print_bytes_hex(uint8_t bytes[], int size)
     printf("\n");
 }
 
-
-void parse_message(uint8_t m[], size_t mlen, int argc, char *argv[])
+void parse_action(char action[], int argc, char *argv[], int action_ind)
 {
-    if (argc > MESSAGE_IND)
+    if (argc > action_ind)
     {
-        memcpy(m, argv[MESSAGE_IND], strlen(argv[MESSAGE_IND]) + 1);
+        memcpy(action, argv[action_ind], strlen(argv[action_ind]) + 1);
+    }
+    else
+    {
+        printf("Not enough arguments provided. Please provide an action "
+               "argument!\n");
+    }
+}
+
+void parse_message(
+    uint8_t m[], size_t mlen, int argc, char *argv[], int message_ind)
+{
+    if (argc > message_ind)
+    {
+        memcpy(m, argv[message_ind], strlen(argv[message_ind]) + 1);
     }
     else
     {
         randombytes(m, mlen);
     }
-
-    printf("Message: %s\n", m);
 }
 
-void parse_random_bytes(uint8_t random_bytes[], size_t rblen, int argc, char *argv[])
+void parse_random_bytes(
+    uint8_t random_bytes[], size_t rblen, int argc, char *argv[],
+    int random_bytes_ind)
 {
-    size_t ind;
-
-    if (argc > RANDOM_BYTES_IND)
+    if (argc > random_bytes_ind)
     {
-        assert((strlen(argv[RANDOM_BYTES_IND]) / 2) == rblen);
-        hex_to_bytes(random_bytes, argv[RANDOM_BYTES_IND]);
+        assert((strlen(argv[random_bytes_ind]) / 2) == rblen);
+        hex_to_bytes(random_bytes, argv[random_bytes_ind]);
     }
     else
     {
         randombytes(random_bytes, rblen);
     }
+}
 
-    printf("Randomness: ");
-    print_bytes_hex(random_bytes, rblen);
-    printf("\n");
+void parse_private_key(
+    uint8_t sk[], int argc, char *argv[], int private_key_ind)
+{
+    if (argc > private_key_ind)
+    {
+        hex_to_bytes(sk, argv[private_key_ind]);
+    }
+    else
+    {
+        printf("Not enough arguments provided! Please provide the private key "
+               "for signing!\n");
+    }
+}
+
+void parse_public_key(
+    uint8_t pk[], int argc, char *argv[], int public_key_ind)
+{
+    if (argc > public_key_ind)
+    {
+        hex_to_bytes(pk, argv[public_key_ind]);
+    }
+    else
+    {
+        printf("Not enough arguments provided! Please provide the public key "
+               "for verification!\n");
+    }
+}
+
+void parse_message_hash(
+    uint8_t sm[], size_t *smlen, int argc, char *argv[], int message_hash_ind)
+{
+    if (argc > message_hash_ind)
+    {
+        hex_to_bytes(sm, argv[message_hash_ind]);
+        *smlen = strlen(argv[message_hash_ind]) / 2;
+    }
+    else
+    {
+        printf("Not enough arguments provided! Please provide the message hash "
+               "for verification!\n");
+    }
 }
 
 int main(int argc, char *argv[])
 {
+    const int ACTION_IND = 1;
+    const int ACTION_LEN = 80;
+
+    const char *ACTION_GENERATE = "generate";
+    const int RANDOM_BYTES_IND = 2;
+
+    const char *ACTION_SIGN = "sign";
+    const int PRIVATE_KEY_IND = 2;
+    const int MESSAGE_IND = 3;
+
+    const char *ACTION_VERIFY = "verify";
+    const int PUBLIC_KEY_IND = 2;
+    const int MESSAGE_HASH_IND = 3;
+
     size_t i;
     int ret;
     size_t mlen, smlen;
+    char action[ACTION_LEN];
     uint8_t m[MLEN + CRYPTO_BYTES];
-    uint8_t m2[MLEN + CRYPTO_BYTES];
+    uint8_t m2[MLEN + CRYPTO_BYTES + 1];
     uint8_t sm[MLEN + CRYPTO_BYTES];
     uint8_t pk[CRYPTO_PUBLICKEYBYTES];
     uint8_t sk[CRYPTO_SECRETKEYBYTES];
     uint8_t random_bytes[SEEDBYTES]; /* 256 */
 
-    /* take message from cmd */
-    /* convert to bytes? */
-    /* temp */
-    /* randombytes(m, MLEN); */
-    /* randombytes(random_bytes, SEEDBYTES); */
-    parse_message(m, MLEN, argc, argv);
-    parse_random_bytes(random_bytes, SEEDBYTES, argc, argv);
-    /* temp */
+    /* get action from cmd */
+    parse_action(action, argc, argv, ACTION_IND);
 
-    /* pass random puf data to function from cmd */
+    if (strcmp(action, ACTION_GENERATE) == 0)
+    {
+        /* parse randomness */
+        parse_random_bytes(
+            random_bytes, SEEDBYTES, argc, argv, RANDOM_BYTES_IND);
 
-    /* generate keypairs, sign, verify */
-    demo_crypto_sign_keypair(random_bytes, pk, sk);
-    crypto_sign(sm, &smlen, m, MLEN, sk);
-    ret = crypto_sign_open(m2, &mlen, sm, smlen, pk);
+        /* generate pairs */
+        demo_crypto_sign_keypair(random_bytes, pk, sk);
 
-    /* terminate m2 for printing */
-    m2[mlen + 1] = '\0';
-
-    if(ret) {
-        fprintf(stderr, "Verification failed\n");
-        return -1;
+        /* print to cmdline */
+        print_bytes_hex(pk, CRYPTO_PUBLICKEYBYTES);
+        print_bytes_hex(sk, CRYPTO_SECRETKEYBYTES);
     }
-    if(smlen != MLEN + CRYPTO_BYTES) {
-        fprintf(stderr, "Signed message lengths wrong\n");
-        return -1;
+    else if (strcmp(action, ACTION_SIGN) == 0)
+    {
+        /* parse private key */
+        parse_private_key(sk, argc, argv, PRIVATE_KEY_IND);
+
+        /* parse message */
+        parse_message(m, MLEN, argc, argv, MESSAGE_IND);
+
+        /* sign */
+        crypto_sign(sm, &smlen, m, MLEN, sk);
+
+        /* print to cmdline */
+        print_bytes_hex(sm, smlen);
     }
-    if(mlen != MLEN) {
-        fprintf(stderr, "Message lengths wrong\n");
-        return -1;
-    }
-    for(i = 0; i < MLEN; ++i) {
-        if(m2[i] != m[i]) {
-            fprintf(stderr, "Messages don't match\n");
+    else if (strcmp(action, ACTION_VERIFY) == 0)
+    {
+        /* parse public key */
+        parse_public_key(pk, argc, argv, PUBLIC_KEY_IND);
+
+        /* parse message hash (extracting size) */
+        parse_message_hash(sm, &smlen, argc, argv, MESSAGE_HASH_IND);
+
+        /* verify */
+        ret = crypto_sign_open(m2, &mlen, sm, smlen, pk);
+
+        /* terminate m2 for printing */
+        m2[mlen + 1] = '\0';
+
+        /* dilithium error checks */
+        if(ret) {
+            printf("Verification failed\n");
             return -1;
         }
+        if(smlen != MLEN + CRYPTO_BYTES) {
+            printf("Signed message lengths wrong\n");
+            return -1;
+        }
+        if(mlen != MLEN) {
+            printf("Message lengths wrong\n");
+            return -1;
+        }
+
+        /* print to cmdline */
+        if (ret == 0) printf("%s\n", m2);
     }
-
-    printf("Public key bytes: %d\n", CRYPTO_PUBLICKEYBYTES);
-    printf("Secret key bytes: %d\n", CRYPTO_SECRETKEYBYTES);
-    printf("Crypto bytes: %d\n", CRYPTO_BYTES);
-
-    printf("\nPublic key:\n");
-    print_bytes_hex(pk, CRYPTO_PUBLICKEYBYTES);
-
-    printf("\nSecret key:\n");
-    print_bytes_hex(sk, CRYPTO_SECRETKEYBYTES);
-
-    printf("\nSignature:\n");
-    print_bytes_hex(sm, smlen);
-
-    if (ret == 0) printf("\nMessage verified successfully!\n");
-    printf("Opened message: %s\n", m2);
 
     return 0;
 }
